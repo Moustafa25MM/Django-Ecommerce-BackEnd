2325db3d8bf050bd7745b38aa7534d696e3cccc7
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-05-04 20:18:04 +0300

    Refactor Get And Post Functions

--- products/views.py
+++ products/views.py
@@ -8,26 +8,22 @@ from rest_framework import status
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    # Post Product
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-class ProductDetail(generics.RetrieveAPIView):
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     # Get Product
-    def retrieve(self, request, pk):
-        product = self.get_object()
-        serializer = self.get_serializer(product)
-        return Response(serializer.data)    
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
     
+    # Update Product
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    # Delete Product
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)  
 
 # Create your views here.
