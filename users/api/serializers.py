73e1d1b8041a830dba2c9132026eac3f0ef31623
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-04 19:28:37 +0300

    customise user Serializer

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -14,12 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['email' , 'username' , 'password' , 'confim_password','image' , 'date_of_birth']
         extra_kwargs = {
             'password':{'write_only':True},
             'confirm_password':{'write_only':True},
         }
         
+    def create(self, validated_data):
+        validated_data.pop('groups')
+        validated_data.pop('user_permissions')
+        validated_data['is_active'] = True
+        return CustomUser.objects.create_user(**validated_data)
     
     def validate(self,data):
         if CustomUser.objects.filter(email=data['email']).exists():
