5a89bc0ddf3f3cf65a13f93f608064739957df7f
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-05 02:57:37 +0300

    added all URLS for user

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from users.api.views import Registeration
+from users.api.views import Registeration , LoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('register/', Registeration.as_view(), name='register'),
+    path('login/',LoginView.as_view(),name='login'),
+    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt_create"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="jwt_verify"),
 ]
