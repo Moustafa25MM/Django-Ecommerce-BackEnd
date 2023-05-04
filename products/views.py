d9013f286f544e0157d097aeff331b9dc7487d19
hossameldinfahmi <hossameldinfahmi@gmail.com>
2023-05-05 00:37:36 +0300

    Refactor Update Product Function

--- products/views.py
+++ products/views.py
@@ -8,22 +8,10 @@ from rest_framework import status
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
-    # Get Product
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-    
-    # Update Product
-    def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-    
-    # Delete Product
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)  
+   
 
 # Create your views here.
