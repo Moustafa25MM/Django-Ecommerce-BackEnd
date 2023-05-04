92d97153df010b4d7aef04f893ba4a5186802775
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-04 17:39:17 +0300

    user Serializers

---
+++ users/api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from users.models import CustomUser , Address
+from django.core.exceptions import ValidationError
+
+class AddressSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Address
+        fields = '__all__'
+        
+    
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        extra_kwargs = {
+            'password':{'write_only':True},
+            'confirm_password':{'write_only':True},
+        }
+        
+    
+    def validate(self,data):
+        if CustomUser.objects.filter(email=data['email']).exists():
+            raise ValidationError('This email is already exists!')
+        
+        if CustomUser.objects.filter(phone=data['phone']).exists():
+            raise ValidationError('This is phone is already registered!')
+        
+        if data['password'] != data['confirm_password']:
+            raise ValidationError("Passwords don't match")
+        
+        return data    
