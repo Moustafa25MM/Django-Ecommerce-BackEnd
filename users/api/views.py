a89ea9fe0bfe944f60c2bc0930180048866f4dd5
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-04 19:37:14 +0300

    added users registeration

---
+++ users/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from api.serializers import Address, CustomUser
+from rest_framework import generics, status
+from api.serializers import AddressSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+
+
+class Registeration (generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes=[AllowAny]
+    
+    def post(self,request:Request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response={
+                "message":"user created Successfully",
+                "data":serializer.data
+            }
+            return Response(data=response , status=status.HTTP_201_CREATED)
+        
+        return Response(data=serializer.errors , status=status.HTTP_400_BAD_REQUEST)
